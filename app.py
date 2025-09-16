8efbe6572bd394fb44f93ab37bc7abbb5c20232d
Ács Dániel <finywow@gmail.com>
2025-09-16 18:12:26 +0200

    added emoji

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello, Render!"
+    return "Hello, Render!🚀"
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
